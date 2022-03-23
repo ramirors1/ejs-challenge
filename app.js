87d4ed279d5bb238cfb63e56b4f216e6f3ccd02d
Your Github User Name <ramirezr@protonmail.com>
2022-03-23 09:22:07 -0500

    finished challenge 8

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const postTitle = [];
+const journalItems = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,8 +23,24 @@ app.get("/", function(req, res){
 
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+
+});
+
+app.post("/compose", function(req, res) {
+  console.log(req.body.postTitle);
+});
 
 
 
