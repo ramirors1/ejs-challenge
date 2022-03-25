b654c15f4b298bf8a175272fb3d0e6cff1e43085
Your Github User Name <ramirezr@protonmail.com>
2022-03-25 11:06:43 -0500

    finished challenge 17

--- app.js
+++ app.js
@@ -50,6 +50,17 @@ app.post("/compose", function(req, res) {
 
 });
 
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = req.params.postName;
+
+  posts.forEach(function(post){
+    const storedTitle = post.title;
+
+    if (storedTitle === requestedTitle) {
+      console.log("Match Found!");
+    }
+  });  
+});
 
 
 
